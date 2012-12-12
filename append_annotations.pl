a57f6fced23d0b4ef855aeedf901f9360fa7d6a8
Adam Cornish <adam.cornish@unmc.edu>
2012-12-12 13:28:15 -0600

    Not really sure what I changed...

--- append_annotations.pl
+++ append_annotations.pl
@@ -1,35 +1,72 @@
 #!/usr/bin/perl
 use warnings;
 use strict;
+use Getopt::Std;
 
-print "Usage: perl $0 directory.\n\tSimply provide the directory that has the *finished.snvs* vcf files in them.\n\n";
-my $dir = ( $ARGV[0] ) ? $ARGV[0] : "./";
-chomp ( my @files = `ls *finished.snvs*` );
+my %opt;
+my @db   = "";
+my $curr = "";
+getopts ( "i:", \%opt );
 
-foreach my $file ( @files )
+print "Usage: perl $0 -i all.vcf.\n\tSimply provide the vcf file that contains the combined annotated results.\n\n";
+my $file = $opt{i};
+# read in the vcf file
+my @data = `grep -vP '^#' $file`;
+# get the names of all of the samples
+chomp ( my $tmp = `grep -P '^#CHROM' $file` );
+$tmp =~ s/#CHROM.+?FORMAT\s//;
+my @names = split /\t/, $tmp;
+open OUT, ">all.variants.txt";
+# regex
+my $important = "(?:NON_SYNONYMOUS_CODING|NON_SYNONYMOUS_START|STOP_GAINED|STOP_LOST|SYNONYMOUS_CODING|SYNONYMOUS_STOP)";
+# set up the initial table
+print OUT "Chromosoms\tPosition\tRef\tAlt\tQuality\tTotal Coverage\tGene Symbol\tExon ID\tEffect\tImpact\tOld_codon/New_codon\tOld_AA/New_AA\tdbSNP ID\tPhyloP\tSIFT\tPolyphen2\tLRT\tMutationTaster\tCOSMIC\t";
+for ( my $i = 0; $i < @names; $i++ )
 {
-    print "Working on $file\n";
-    my @db   = "";
-    my @data = `grep -v '^#' $file`;
-    my $curr = "";
-    my ($name ) = $file =~ /(.+?)\./;
-    open OUT, ">$name.snvs.txt";
-    print OUT "Chromosome\tPosition\tdbSNP ID\tRef\tAlt\tMutation\tImpact\tType\tCodon Change\tAA Change\tGene\tAccession ID\tExon\tPhyloP\tSIFT\tPolyphen2\tLRT\tMutationTaster\n";
+    my $name = $names[$i];
+    print OUT "Ref depth-$name\tAlt depth-$name\t% depth-$name";
+    ( $i == $#names) ? print OUT "\n" : print OUT "\t";
+}
 
-    foreach my $datum ( @data )
+# Start looping through the lines in the vcf file
+foreach my $datum ( @data )
+{
+    my @split = split /\t/, $datum;
+    $split[2] =~ s/\./N\\a/;
+    my $meta = $split[7];
+    my ($eff) = $split[7] =~ /EFF=(\S+)/;
+    my @effects = split /,/, $eff;
+    my ($DP) = $datum =~ /DP=(\d+)/;
+    my $pre = "$split[0]\t$split[1]\t$split[3]\t$split[4]\t$split[5]\t$DP\t";
+    my $post = "";
+    my $chr = $split[0];
+    if ( $curr ne $chr )
     {
-        my ($chr)   = $datum =~ /^(\S+)/;
-        if ( $curr ne $chr )
+        print "\tReading in $chr.\n";
+        @db = `cat /safer/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/$chr | grep -vP '^#'`;
+        $curr = $chr;
+    }
+    for ( my $i = 9; $i < @split; $i++ )
+    {
+        if ( $split[$i] =~ /\.\/\./ )
         {
-            print "\tReading in $chr.\n";
-            @db = `cat /data/genomes/Homo_sapiens/UCSC/hg19/Annotation/Variation/dbNSFP/$chr | grep -vP '^#'`;
-            $curr = $chr;
+            $post .= "0\t0\t0.00%";
         }
-        my @split   = split "\t", $datum;
-        my ($eff)   = $datum =~ /EFF=(.+?)\s/;
-        my @effects = split ",", $eff;
-        foreach my $effect ( @effects )
+        else
         {
+            my ( $ref, $alt) = $split[$i] =~ /.+?:(\d+),(\d+)/;
+            my $percent = "0\%"; # assume you will divide by 0
+            $percent = sprintf ( "%.2f%%", ($alt/($ref+$alt))*100 ) unless ( $ref + $alt == 0 );
+            $post .= "$ref\t$alt\t$percent";
+        }
+        $post .= ( $i == $#split) ? "\n" : "\t";
+    }
+    foreach my $effect ( @effects )
+    {
+        if ( $effect =~ /$important/ )
+        {
+            my ($type, $impact, $codon_change, $AA_change, $gene_symbol, $exon_ID) = $effect =~ /(.+?)\((.*?)\|.*?\|(.*?)\|(.*?)\|.*?\|(.*?)\|.*?\|.*?\|.*?\|(.*?)\)/;
+            print OUT "$pre$gene_symbol\t$exon_ID\t$type\t$impact\t$codon_change\t$AA_change\t$split[2]\t$post";
             my ($loc, $parens) = $effect =~ /(.+?)\((.+?)\)/;
             my ($impact,$type,$codon,$aa,$gene,$rna,$coding,$acc,$exon) = split /\|/, $parens;
             print OUT "$split[0]\t$split[1]\t$split[2]\t$split[3]\t$split[4]\t$loc\t$impact\t$type\t$codon\t$aa\t$gene\t$acc\t$exon";
@@ -45,8 +82,8 @@ foreach my $file ( @files )
                     print OUT $out;
                 }
             }
-            print OUT "\n";
         }
+        print OUT "\n";
     }
 
     close OUT;
